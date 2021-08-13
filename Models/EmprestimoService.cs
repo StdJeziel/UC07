f855952c62c3b7401eec4e89e6508c0213326ba9
Jeziel <jeziel@dominio.com.br>
2021-08-13 00:44:32 -0300

    Correçao da funçao de pesquisa - Listagem de Emprestimo

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Biblioteca.Models
 {
@@ -30,14 +31,38 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro = null)
         {
             using (BibliotecaContext bc = new BibliotecaContext())
             {
-                IQueryable<Emprestimo> emprestimos =
-                    bc.Emprestimos.Include(e => e.Livro).OrderByDescending(e => e.DataDevolucao);
-                
-                return emprestimos.ToList();
+                IQueryable<Emprestimo> emprestimos;
+
+                if (filtro != null)
+                {
+                    switch (filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            emprestimos =
+                                bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro, StringComparison.OrdinalIgnoreCase));
+                            break;
+
+                        case "Livro":
+                            emprestimos = 
+                                bc.Emprestimos.Where(e => e.Livro.Titulo.Contains(filtro.Filtro, StringComparison.OrdinalIgnoreCase));
+                            break;
+
+                        default:
+                            emprestimos = bc.Emprestimos;
+                            break;
+                    }
+                }
+
+                else
+                {
+                    emprestimos = bc.Emprestimos;
+                }
+
+                return emprestimos.Include(e => e.Livro).OrderByDescending(e => e.DataDevolucao).ToList();
             }
         }
 
